b09db9fb53d8cdde56e68adc59b604f99c8a4443
gary.moran <gary.moran@obs-logistics.com>
2020-06-28 17:13:55 +0100

    20200628

--- index.js
+++ index.js
@@ -48,19 +48,62 @@ Vue.component("choices-display", {
 		category: {type: Boolean}		
     },
     template:
-    `
-	<div class="card">
-	    <div class="card-header">
-            {{choiceText}}
-			<span class="float-right badge badge-info" v-if="choiceCategory">
-				CATEGORY
-			</span>
+    `    
+    <div>
+        <!-- Card -->
+        <div class="card">
+            <div class="card-header" style="transform: rotate(0);">
+                <a href="#" class="stretched-link" v-on:click="selectChoice"></a>
+                {{choiceText}}
+                <span class="float-right badge badge-info" v-if="choiceCategory">
+                    CATEGORY
+                </span>
+            </div>
+            <div class="card-body">
+                <button class="btn btn-success" data-toggle="modal" data-target="#addChoice">Add New Choice</button>
+                <button class="btn btn-danger" v-if="canDelete" v-on:click="deleteData">Delete this Choice</button>				
+            </div>
         </div>
-		<div class="card-body">
-			<button class="btn btn-success">Add</button>
-			<button class="btn btn-danger">Delete</button>				
-		</div>
-	</div>
+        
+        <!-- Modal Save -->
+        <div class="modal fade" id="addChoice" tabindex="-1" role="dialog" aria-labelledby="addNewChoiceLabel"
+        aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addNewChoiceLabel">Add New Choice</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label>Choice Text
+                                <input type="text" class="form-control" v-model="addText">
+                                <small class="form-text text-muted">The text that describes your choice</small>
+                            </label>
+                        </div>
+                        <div class="form-group">
+                            <label>Choice Image URL
+                                <input type="url" class="form-control" v-model="addImage">
+                                <small class="form-text text-muted">An image URL for your choice</small>
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <label class="form-check-label">
+                                <input type="checkbox" class="form-check-input" v-model="checked">
+                                Category
+                            </label>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>                                                        
+                        <button type="button" class="btn btn-primary" v-on:click="saveData" data-dismiss="modal">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     `,
     created() {        
     },
@@ -70,78 +113,45 @@ Vue.component("choices-display", {
             choiceParent: this.parent,
             choiceText: this.text,
             choiceImage: this.image,   
-            choiceCategory: this.category         
-        }
-    },
-    computed: {
-    },
-    methods: {
-    }
-})
-
-Vue.component("choices-add", {
-    props: {
-        keyProp: {type: String, default: ""},
-        parent: {type: String, default: ""}
-    },
-    template:
-    `
-    <form>
-        <div class="form-group">
-            <label>Choice Text
-                <input type="text" class="form-control" v-model="choiceText">
-                <small class="form-text text-muted">The text that describes your choice</small>
-            </label>					
-        </div>
-        <div class="form-group">
-            <label>Choice Image URL
-                <input type="url" class="form-control" v-model="choiceImage">
-                <small class="form-text text-muted">An image URL for your choice</small>
-            </label>
-        </div>
-        <div class="form-check">
-            <label class="form-check-label">
-                <input type="checkbox" class="form-check-input" v-model="checked">
-                Category						
-            </label>
-        </div>
-        <button type="submit" class="btn btn-primary">Save</button>
-    </form>
-    `,
-    created() {        
-    },
-    data: function () {
-        return {
-            choiceKey: this.keyProp,
-            choiceParent: this.parent,
-            choiceText: "",
-            choiceImage: "",
+            choiceCategory: this.category,
+            addKey: "",
+            addText: "",
+            addImage: "",
             checked: false
         }
     },
     computed: {
-         choiceCategory() {
-            return this.checked;
-        }
-    },
-    methods: {
-         saveData() {
-            data = {
-                parent: this.choiceParent,
-                text: this.choiceText,
-                image: this.choiceImage,
-                category: this.choiceCategory
-            };
-            this.choicekey = saveData(this.choiceKey, data);
-            //this.addMessage(`${this.choiceText} choice successfully saved`);
+        addCategory() {
+           return this.checked;
+       },
+       canDelete() {
+           return this.choiceKey != "CHC:ROOT";
+       }
+   },
+   methods: {
+        selectChoice() {
+            this.$emit("select-choice", this.choiceKey)
         },
-        deleteData() {
-            var letName = this.$refs.Name.inputData;
-            deleteData(this.$refs.Id.inputData);
-            this.newData();
-            //this.addMessage(`Deleted choice: ${this.choiceText}`);
-        }
-    }
+        saveData() {
+           data = {
+               parent: this.choiceKey,
+               text: this.addText,
+               image: this.addImage,
+               category: this.addCategory
+           };
+           this.choicekey = saveData(this.addKey, data);
+           this.$emit("add-message",`${this.addText} choice successfully saved`);
+           // clean-up
+           this.addText = "";
+           this.addImage = "";
+           this.checked = false;
+       },
+       deleteData() {
+           deleteData(this.choiceKey);           
+           this.$emit("add-message",`Deleted choice: ${this.choiceText}`);
+           this.$emit("select-choice", this.choiceParent)
+       }
+   }
 })
 
 // Vue App
@@ -149,15 +159,59 @@ var app = new Vue({
     el: '#app',
      data: function () {
         return {
-            appList: []
+            rootKey: "CHC:ROOT",
+            appList: [],
+            messages: [],
+            parent: ""
         }
     },
     created() {
-        let key = "CHC:ROOT";
-        data = getRootData(key);
-        this.appList = [ { key: key, data: data } ]
+        this.goRoot();
     },
+    computed: {
+        atRoot() {
+            return this.parent === "";
+        },
+        haveMessages() {
+            return this.messages.length > 0;
+        },        
+    },    
     methods: {
+        goRoot() {
+            let key = this.rootKey;
+            let data = getRootData(key);
+            this.appList = [ { key: key, data: data } ]            
+        },
+        goBack() {
+            if (this.parent !== "") {
+                let data = getData(this.parent);
+                if (data !== undefined || data !== null) {
+                    if (data.parent !== undefined || data.parent !== null) 
+                        this.selectChoice(data.parent);
+                    else 
+                        this.goRoot();
+                }
+                else
+                    this.goRoot();
+            }
+        },
+        selectChoice(choiceKey) {
+            let list = listData(choiceKey);
+            if (list && list.length) {
+                this.appList = list;
+                this.parent = choiceKey;
+            }
+        },
+        addMessage(messageText, messageType) {
+            this.messages.push({ text: messageText, alertClass: getAlertClass(messageType) });            
+            this.timer = setTimeout(this.clearMessages, 3000);
+        },
+        clearMessages() {
+            for (i = 0; i < this.messages.length; i++) {
+                if (this.messages.alertClass !== "danger")
+                    this.messages.splice(i, 1);
+            }
+        }        
     }
 });
 
@@ -186,17 +240,21 @@ function getRootData(rootKey) {
 /**
  * Returns Choices data as a list
  * 
+ * @param {string} choiceKey
  * @return {Array} key data objects
  */
-function listData() {
+function listData(choiceKey) {
     var list = [];
+
     Object.keys(localStorage).forEach(function (key) {
         if (key.startsWith("CHC:")) {
             let data = JSON.parse(localStorage.getItem(key));
-            let item = {
-                key: key, text: data.text, image: data.image, checked: data.checked
-            }
-            list.push(item);
+            if (data.parent === choiceKey) {                
+                let item = {
+                    key: key, data: data
+                }
+                list.push(item);
+            }            
         }
     });
     return list;
@@ -259,4 +317,26 @@ function matchObjectProperties(object, filterRegEx) {
             return true;
     }        
     return false;
+}
+
+/**
+ * Returns a Bootstrap alert class for a message type
+ * 
+ * @param {string} messageType 
+ * @return {string} Bootstrap alert class
+ */
+function getAlertClass(messageType) {
+
+    var alertClass = 'alert';
+    if (messageType == "info")
+        alertClass += ' alert-info';
+    else if (messageType == "warning")
+        alertClass += ' alert-warning';
+    else if (messageType == "danger")
+        alertClass += ' alert-danger';
+    else
+        alertClass += ' alert-success';
+    alertClass += ' alert-dismissible hide alerts-fade-in';
+
+    return alertClass;
 }
